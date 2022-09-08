c3833bd57d7533472f19d7697de1bd455085a6fc
AndreyLytvynov <mrjastonehome@gmail.com>
2022-09-08 18:07:56 +0300

    Исправлены селекторы по БЕМ, добавлен SASS

--- js/test.js
+++ js/test.js
@@ -8,10 +8,10 @@ refs.openModalBtn.addEventListener('click', toggleModal);
 refs.closeModalBtn.addEventListener('click', toggleModal);
 
 function toggleModal() {
-  refs.modal.classList.toggle('closed');
+  refs.modal.classList.toggle('backdrop--closed');
 }
 
-document.querySelector('.form').addEventListener('submit', e => {
+document.querySelector('.form-hero').addEventListener('submit', e => {
   e.preventDefault();
 
   new FormData(e.currentTarget).forEach((value, name) => console.log(`${name}: ${value}`));
